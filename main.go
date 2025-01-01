d40118e250a85da978b502f51b03defcc4da6b7a
Isaque Franklin <isaquefrankli@gmail.com>
2025-01-01 18:46:01 -0300

    Adding route to main

--- main.go
+++ main.go
@@ -60,6 +60,8 @@ func main() {
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handleGetUser))
 
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
