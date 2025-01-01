7aa62bf2a125af675f41f8995cd987c737fbf70b
Isaque Franklin <isaquefrankli@gmail.com>
2025-01-01 17:33:32 -0300

    Some auth stuff.

--- handler_user.go
+++ handler_user.go
@@ -7,12 +7,13 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/isaquefranklin/rss-scraper/auth"
 	"github.com/isaquefranklin/rss-scraper/internal/database"
 )
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Name string "json:`name`"
+		Name string `json:"name"`
 	}
 	decoder := json.NewDecoder(r.Body)
 
@@ -34,30 +35,19 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	respondWithJSON(w, 200, databaseUserToUser(user))
+	respondWithJSON(w, 201, databaseUserToUser(user))
 }
 
 func (apiCfg *apiConfig) handleGetUser(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Name string "json:`name`"
-	}
-	decoder := json.NewDecoder(r.Body)
-
-	params := parameters{}
-	err := decoder.Decode(&params)
+	apikey, err := auth.GetAPIKey(r.Header)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %w", err))
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %w", err))
 		return
 	}
 
-	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now().UTC(),
-		UpdatedAt: time.Now().UTC(),
-		Name:      params.Name,
-	})
+	user, err := apiCfg.DB.GetUserByApiKey(r.Context(), apikey)
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %s", err))
+		respondWithError(w, 404, fmt.Sprintf("Couldn't get user: %w", err))
 		return
 	}
 
